76e6b38d8d095282e79d1325b870dbd0f9877092
Martin <martin.zackrisson@fripost.org>
2026-03-29 13:23:08 +0200

    Add health station

---
+++ clicker/health_station.gd
@@ -0,0 +1,35 @@
+extends Node3D
+class_name HealthStation
+
+@export var healing_amount: float = 10
+
+const _DEACTIVATION_COUNT: int = 3
+static var _inactive_stations: Array[HealthStation]
+
+var inactive: bool:
+    get():
+        return _inactive_stations.has(self)
+
+func _on_area_3d_area_entered(area: Area3D) -> void:
+    var player: PhysicsGridPlayerController = PhysicsGridPlayerController.find_in_tree(area)
+    print_debug("Player %s entered healing station %s == %s" % [player, __GlobalGameState.health, __GlobalGameState.max_health])
+    if player == null || __GlobalGameState.health == __GlobalGameState.max_health:
+        return
+
+    if inactive:
+        return
+
+    print_debug("Healing player %s by %s" % [player, healing_amount])
+    __GlobalGameState.health += healing_amount
+    _deactivate()
+
+func _deactivate() -> void:
+    _inactive_stations.append(self)
+
+    while !_inactive_stations.is_empty() && _inactive_stations.size() > _DEACTIVATION_COUNT:
+        var station: HealthStation = _inactive_stations[0]
+        _inactive_stations.remove_at(0)
+        station._activate()
+
+func _activate() -> void:
+    pass

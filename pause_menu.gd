0494894da00c28692cfdfb2f71d88afe8b9d0dc4
Martin <martin.zackrisson@fripost.org>
2026-04-04 22:23:04 +0200

    Add audio settings to pause menu

--- pause_menu.gd
+++ pause_menu.gd
@@ -5,12 +5,16 @@ signal closed
 
 @export var menu_container: VBoxContainer
 @export var gfx_container: VBoxContainer
+@export var audio_container: VBoxContainer
 @export var quit_container: VBoxContainer
 @export var clicker_env: Environment
 @export var horror_env: Environment
 @export var brightness_slider: HSlider
 @export var contrast_slider: HSlider
 @export var saturation_slider: HSlider
+@export var music_slider: HSlider
+@export var dialogue_slider: HSlider
+@export var sfx_slider: HSlider
 @export var start_menu_scene: PackedScene
 
 
@@ -37,6 +41,7 @@ func _on_back_btn_pressed() -> void:
     menu_container.show()
     gfx_container.hide()
     quit_container.hide()
+    audio_container.hide()
 
 
 func _on_brightness_slider_value_changed(value: float) -> void:
@@ -61,3 +66,24 @@ func _on_quit_desktopbtn_pressed() -> void:
 
 func _on_quit_main_menubtn_pressed() -> void:
     get_tree().change_scene_to_packed(start_menu_scene)
+
+
+func _on_audio_btn_pressed() -> void:
+    music_slider.value = __AudioHub.get_volume(AudioHub.Bus.MUSIC)
+    dialogue_slider.value = __AudioHub.get_volume(AudioHub.Bus.DIALGUE)
+    sfx_slider.value = __AudioHub.get_volume(AudioHub.Bus.SFX)
+
+    menu_container.hide()
+    audio_container.show()
+
+
+func _on_music_volume_slider_value_changed(value: float) -> void:
+    __AudioHub.set_volume(AudioHub.Bus.MUSIC, value)
+
+
+func _on_dialogue_volume_slider_value_changed(value: float) -> void:
+    __AudioHub.set_volume(AudioHub.Bus.DIALGUE, value)
+
+
+func _on_sfx_volume_slider_value_changed(value: float) -> void:
+    __AudioHub.set_volume(AudioHub.Bus.SFX, value)

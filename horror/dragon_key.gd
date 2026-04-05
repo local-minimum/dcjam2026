ab396321f713630e044f5a18030f81e6ea69ac72
Martin <martin.zackrisson@fripost.org>
2026-04-05 16:40:00 +0200

    Add one more dragon key

--- horror/dragon_key.gd
+++ horror/dragon_key.gd
@@ -4,6 +4,16 @@ class_name DragonKey
 @export var key: Node3D
 @export var col: CollisionShape3D
 
+func _enter_tree() -> void:
+    if __SignalBus.on_collect_horror_key.connect(_inc_keys_collected) != OK:
+        push_error("Failed to connect")
+
+var _collected: int = 0
+func _inc_keys_collected() -> void:
+    _collected += 1
+    if key.visible && _collected >= 4:
+        key.hide()
+
 func _on_area_3d_area_entered(area: Area3D) -> void:
     var player: PhysicsGridPlayerController = PhysicsGridPlayerController.find_in_tree(area)
     if key.visible && player != null && !player.cinematic && player.grid_entity.detection_areas.has(area):

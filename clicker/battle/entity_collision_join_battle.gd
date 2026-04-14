460d5afd361efa864ba215aee2f62bfd727994bc
Martin Zackrisson <martin.zackrisson@fripost.org>
2026-04-14 21:55:32 +0200

    Fix bugs colliding with looting visible

--- clicker/battle/entity_collision_join_battle.gd
+++ clicker/battle/entity_collision_join_battle.gd
@@ -2,16 +2,20 @@ extends EntityCollisionResolutionSystem
 
 func _resolve_collision(a: GridEntity, b: GridEntity) -> void:
     if b.player != null && a.type == GridEntity.EntityType.ENEMY:
-        if !_add_enemy_to_battle(a as StationaryEnemy):
+        if !await _add_enemy_to_battle(a as StationaryEnemy):
             push_error("Could not add %s to battlee because not a stationary enemy" % [a])
 
     elif a.player != null && b.type == GridEntity.EntityType.ENEMY:
-        if !_add_enemy_to_battle(b as StationaryEnemy):
+        if !await _add_enemy_to_battle(b as StationaryEnemy):
             push_error("Could not add %s to battlee because not a stationary enemy" % [b])
+
 
 func _add_enemy_to_battle(e: StationaryEnemy) -> bool:
     if e == null:
         return false
+
+    while LootAndInventory.looting_in_progress:
+        await get_tree().create_timer(0.5, false).timeout
 
     __SignalBus.on_enemy_join_battle.emit(e.data)
     e.deactivate()
